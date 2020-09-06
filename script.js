6418ba947d89777c992fd043e8aa7cf557e54643
timsilby <69242373+timsilby@users.noreply.github.com>
2020-09-06 18:42:18 +1000

    Updating script.js with functions: startTimer startQuiz runQuiz, placeholders for other functions

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var arrQuestions = [
 	{
 		question: "What is the correct syntax for referring to an external script called script.js?",
 		options: ["<script src='script.js'>", "<script href='script.js'>", "<script ref='script.js'>", "<script name='script.js'>"],
-		answer: "<script src='script.js'>"
+		answer: 0
 	}
 
 ];
@@ -28,6 +28,7 @@ function startQuiz(event) {
 	elQuizDisplay.style.display = "block";
 
 	startTimer();
+	runQuiz();
 
 }
 
@@ -41,15 +42,66 @@ function startTimer() {
 	elTimer.textContent = intSecondsLeft;
 
 	if (intSecondsLeft === 0) {
-		// endQuiz("timeout");
+
+		endQuiz("timeout");
 		clearInterval(quizTimer);
 		console.log("finished");
+
 	}
 
 	}, 1000);
 
 }
 
+
+function runQuiz() {
+
+	for (let i = 0; i < arrQuestions.length; i++) {
+
+		renderQuestion(i);
+
+		if (getResult())
+		{
+			updateScore();
+		}
+		else
+		{
+			updateTimer();
+		}
+
+	}
+
+	if (i === arrQuestions.length - 1) {
+
+		endQuiz("complete");
+
+	}
+}
+
+
+function renderQuestion() {
+	console.log("fnc renderQuestion");
+}
+
+
+function getResult() {
+	console.log("fnc getResult");
+}
+
+
+function updateScore() {
+	console.log("fnc updateScore");
+}
+
+
+function updateTimer() {
+	console.log("fnc updateTimer");
+}
+
+
+function endQuiz(reason) {
+	console.log("fnc endQuiz");
+}
 
 elStartQuizBtn.addEventListener("click", startQuiz);
 
