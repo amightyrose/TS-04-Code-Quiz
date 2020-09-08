881e5fd379d6bbee70bfe06403265fb06ca25542
timsilby <69242373+timsilby@users.noreply.github.com>
2020-09-08 16:44:04 +1000

    Filling out the questions array

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var elQuestionText = document.querySelector("#questiontext")
 var elAnswerList = document.querySelector("#answerlist")
 
 var quizTimer;
-var intSecondsLeft = 5;
+var intSecondsLeft = 60;
 var intQuestionIndex = 0;
 var strCorrectAnswer;
 
@@ -17,10 +17,79 @@ var arrQuestions = [
 		question: "What is the correct syntax for referring to an external script called script.js?",
 		options: ["<script src='script.js'>", "<script href='script.js'>", "<script ref='script.js'>", "<script name='script.js'>"],
 		answer: "0"
+	},
+    {
+        question: "Commonly used data types DO NOT include:",
+        options: ["alerts", "booleans", "numbers", "strings"],
+        answer: "0"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within __________",
+        options: ["curly braces", "parentheses", "quotes", "square brackets"],
+        answer: "1"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store __________",
+        options: ["booleans", "numbers and strings", "other arrays", "all of the above"],
+        answer: "3"
+    },
+    {
+        question: "String values must be enclosed within __________ when being assigned to variables.",
+        options: ["commas", "curly braces", "quotes", "parentheses"],
+        answer: "2"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the degugger is:",
+        options: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "3"
+	},
+	{
+		question: "True or false: an external JavaScript file must contain a <script> tag.",
+		options: ["true", "false"],
+		answer: "1"
+	},
+	{
+		question: "Which of the following is the correct syntax to display 'I love JavaScript' in an alert box using JavaScript?",
+		options: ["alertbox('I love JavaScript')", "msg('I love JavaScript')", "msgbox('I love JavaScript')", "alert('I love JavaScript')"],
+		answer: "3"
+	},
+	{
+		question: "What is the correct syntax for adding comments in JavaScript?",
+		options: ["<!–This is a comment–>", "//This is a comment", "–This is a comment", "**This is a comment**"],
+		answer: "1"
+	},
+	{
+		question: "Which method is used to remove the whitespace at the beginning and end of any string?",
+		options: ["strip()", "trim()", "stripped()", "trimmed()"],
+		answer: "1"
+	},
+	{
+		question: "True or false: the <script> tag can be used to add JavaScript to an HTML document.",
+		options: ["true", "false"],
+		answer: "0"
+	},
+	{
+		question: "How do you create a function in JavaScript?",
+		options: ["function someFunction()", "function = someFunction()", "function:someFunction()"],
+		answer: "0"
+	},
+	{
+		question: "How do you call a function named 'someFunction'?",
+		options: ["call function someFunction()", "call someFunction()", "someFunction"],
+		answer: "2"
+	},
+	{
+		question: "True or false: JavaScript is the the same as Java.",
+		options: ["true", "false"],
+		answer: "1"
+	},
+	{
+		question: "Which operator is used to assign a value to a variable?",
+		options: ["*", ">", "=", ":"],
+		answer: "2"
 	}
 
 ];
-
 
 
 
